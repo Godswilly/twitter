eba7a7c71f8c67e0c0a949ffe7ece673d3e9c6ea
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2020-10-05 11:15:50 +0100

    Add the post head

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,12 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :set_tweet, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+
 
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order('created_at DESC')
   end
 
   # GET /tweets/1
