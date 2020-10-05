b5e9056c5af2fd332b823082309b5524bfd695ec
Kalu Agu Kalu <kalu.agukalu@gmail.com>
2020-10-05 15:39:04 +0100

    Add layout and markup for devise user model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,11 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: %i[index show]
-
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all.order('created_at DESC')
+    @tweets = Tweet.all.order("created_at DESC")
+    @tweet = Tweet.new
   end
 
   # GET /tweets/1
@@ -30,7 +29,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
